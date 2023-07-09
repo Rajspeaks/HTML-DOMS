65c1c65509d32e855c3b6bd97111a7b9ffa2ffdd
Rajdeep Das <dasrajdeep0306@gmail.com>
2023-07-09 20:39:32 +0530

    Updated header.js

--- components/header.js
+++ components/header.js
@@ -36,7 +36,6 @@ headerTemplate.innerHTML = `
       <ul>
         <li><a href="index.html">Home</a></li>
         <li><a href="about.html">About</a></li>
-        <li><a href="work.html">Work</a></li>
         <li><a href="contact.html">Contact</a></li>
       </ul>
     </nav>
